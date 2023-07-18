1f21ead1ad29e99111906b7da1f15c6d5da3a8fa
oqoqoh <oqoq59@gmail.com>
2023-07-19 01:31:30 +0900

    add callback props at Counter.jsx

--- src/AppCounter.jsx
+++ src/AppCounter.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './components/Counter';
 import './App.css';
 
 export default function AppCounter() {
+    const [totalCount, setTotalCount] = useState(0);
+    const handleClick = () => setTotalCount((prev) => prev + 1);
     return (
-        <div>
-            <Counter></Counter>
+        <div className="container">
+            <h1>
+                Total Count :
+                <span>
+                    {totalCount}
+                    {totalCount >= 10 ? '🔥' : '❄️'}
+                </span>
+            </h1>
+            <Counter onClick={handleClick} totalCount={totalCount} />
+            <Counter onClick={handleClick} totalCount={totalCount} />
         </div>
     );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
 
-export default function Counter() {
+export default function Counter({ onClick, totalCount }) {
     const [count, setCount] = useState(0);
+
     return (
         <div className="counter">
-            <span className="number">{count}</span>
+            <span className="number">
+                {count}
+                <span className="total">/{totalCount}</span>
+            </span>
+
             <button
                 className="btn"
                 onClick={() => {
-                    setCount(count + 1);
+                    //setCount(count + 1);
                     setCount((prev) => prev + 1);
+                    onClick(totalCount + 1);
                 }}>
                 add
             </button>
