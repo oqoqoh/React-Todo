cc78774ffaeb0e1e20f3ed3674fd3c2ecf18e025
oqoqoh <oqoq59@gmail.com>
2023-08-12 23:31:46 +0900

    💄 css on component

---
+++ src/AppTheme.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import './AppTheme.css';
+import { DarkModeProvider } from './context/DarkModeContext';
+
+export default function AppTheme() {
+    return (
+        <DarkModeProvider>
+            <Header />
+            <Main />
+            <Footer />
+        </DarkModeProvider>
+    );
+}
+
+function Header() {
+    return <header className="header">Header</header>;
+}
+
+function Main() {
+    return (
+        <main className="main">
+            Main
+            <Profile />
+            <Products />
+        </main>
+    );
+}
+
+function Profile() {
+    return (
+        <div>
+            Profile
+            <User />
+        </div>
+    );
+}
+
+function User() {
+    return <div>User</div>;
+}
+
+function Products() {
+    return (
+        <div>
+            Products
+            <ProductDetail />
+        </div>
+    );
+}
+
+function ProductDetail() {
+    const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
+    return (
+        <div>
+            Product Detail
+            <p>
+                DarkMode:
+                {darkMode ? <span style={{ backgroundColor: 'black', color: 'white' }}>Dark</span> : <span>Light</span>}
+            </p>
+            <button onClick={() => toggleDarkMode()}>Toggle</button>
+        </div>
+    );
+}
+
+function Footer() {
+    return <footer>Footer</footer>;
+}
