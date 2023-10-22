a212675a700978324dbe15a5110f06c250b84a6e
oqoqoh <oqoq59@gmail.com>
2023-10-23 01:52:01 +0900

    ➕ useNavigate() 적용

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Videos from './pages/Videos';
+import Root from './pages/Root';
+import VideoDetail from './pages/VideoDetail';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        errorElement: <NotFound />,
+        children: [
+            { index: true, element: <Home /> },
+            { path: '/videos', element: <Videos /> },
+            { path: '/videos/:videoId', element: <VideoDetail /> },
+        ],
+    },
+]);
+function App() {
+    return <RouterProvider router={router} />;
+}
+export default App;
---
+++ src/pages/Videos.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function Videos() {
+    const navigate = useNavigate();
+    const [text, setText] = useState('');
+    const handleChange = (e) => {
+        setText(e.target.value);
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setText('');
+        navigate(`/videos/${text}`);
+    };
+    return (
+        <div>
+            Videos
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={text} onChange={handleChange} />
+            </form>
+        </div>
+    );
+}
